7a9e97daad244904d08377bd3224afbb4767fd49
tasnim63 <tamanna202163@gmail.com>
2022-06-14 21:34:31 +0600

    project updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import AOS from "aos";
 import React from "react";
-import resume from "../../assets/resume/Tamanna.docx (5).pdf";
+import resume from "../../assets/resume/Tamanna.docx (6).pdf";
 import About from "../About/About";
 import AllProjects from "../Projects/AllProjects";
 import Skills from "./Skills";
